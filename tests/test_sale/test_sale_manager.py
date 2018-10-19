85de27a7824d3b74788d53b4efc36be550b2927c
myco <coditmyco6@gmail.com>
2018-10-19 22:03:10 +0300

    [Finishes #161335560] write tests for sale manager class

---
+++ tests/test_sale/test_sale_manager.py
@@ -0,0 +1,15 @@
+from tests.fixture import FixtureTest
+from flasky.sale.manager import sale_manager
+
+
+class TestSaleManager(FixtureTest):
+    """
+    Test sale manager initialisation
+    """
+
+    def test_sale_manager_init(self):
+        self.assertIsInstance(sale_manager.sale_records, dict)
+
+    def test_add_new_sale_increases_sale_records_count(self):
+        sale_manager.add_sale_record(self.sale)
+        self.assertEqual(len(sale_manager.sale_records), 1)
