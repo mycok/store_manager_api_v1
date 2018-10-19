db172bc7562cd9c7a4a2a3b9c7a668ea77c0938b
myco <coditmyco6@gmail.com>
2018-10-19 21:54:47 +0300

    [Finishes #161335530] write tests for sale model

---
+++ tests/test_sale/test_sale_model.py
@@ -0,0 +1,16 @@
+from tests.fixture import FixtureTest
+from flasky.sale.model import Sale
+
+
+class TestSaleModel(FixtureTest):
+    """
+    Test sale initialisation
+    """
+
+    def test_sale_init(self):
+        self.assertEqual(self.sale.attendant, 'kibuuka')
+        self.assertIsInstance(self.sale, Sale)
+
+    def test_convert_sale_to_json(self):
+        dict_sale = self.sale.to_json()
+        self.assertIsInstance(dict_sale, dict)
