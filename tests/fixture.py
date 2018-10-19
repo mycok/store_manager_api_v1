b3cbe9af33b24d088d32aea1242da3d5f02221cf
myco <coditmyco6@gmail.com>
2018-10-19 22:11:08 +0300

    Add sale product test template methods

--- tests/fixture.py
+++ tests/fixture.py
@@ -18,7 +18,6 @@ class FixtureTest(TestCase):
         self.product = Product('macbook air', 'computers/laptops', 3, 1499.0)
         self.sale = Sale('kibuuka')
 
-
     def tearDown(self):
         product_manager.products.clear()
 
@@ -50,3 +49,28 @@ class FixtureTest(TestCase):
         """
         return self.client.get(
              '/api/v1/products', content_type='application/json')
+
+# sale helper methods
+    def create_sale(self):
+        """
+        send a POST request to create a sale object
+        """
+        return self.client.post(
+            '/api/v1/sales', content_type='application/json',
+            data=json.dumps(dict(attendant='michael')))
+
+    def create_sale_without_the_attendant_attribute(self):
+        """
+        send a POST request to create a sale object
+        with  a missing required parameter.
+        """
+        return self.client.post(
+            '/api/v1/sales', content_type='application/json',
+            data=json.dumps(dict(attendant='  ')))
+
+    def get_all_sales(self):
+        """
+        send a GET request to fetch all sales
+        """
+        return self.client.get(
+             '/api/v1/sales', content_type='application/json')
