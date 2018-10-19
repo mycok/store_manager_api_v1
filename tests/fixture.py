886b2d0607c09fdf91a1980dca538be2d192fb6b
myco <coditmyco6@gmail.com>
2018-10-19 21:32:53 +0300

    Add product model test template functions

--- tests/fixture.py
+++ tests/fixture.py
@@ -3,6 +3,7 @@ import json
 from flasky import create_app
 from flasky.config import TestingConfig
 from flasky.product.model import Product
+from flasky.product.manager import product_manager
 
 
 class FixtureTest(TestCase):
@@ -16,4 +17,33 @@ class FixtureTest(TestCase):
         self.product = Product('macbook air', 'computers/laptops', 3, 1499.0)
 
     def tearDown(self):
-        pass
+        product_manager.products.clear()
+
+    # Product Model helper methods
+    def create_product(self):
+        """
+        send a POST request to create a product object
+        """
+        return self.client.post(
+            '/api/v1/products', content_type='application/json',
+            data=json.dumps(dict(name='macbook air',
+                            category='computers/laptops',
+                            quantity=4, price=1499.0)))
+
+    def create_product_with_missing_attributes(self):
+        """
+        send a POST request to create a product object
+        with missing required parameters.
+        """
+        return self.client.post(
+            '/api/v1/products', content_type='application/json',
+            data=json.dumps(dict(name='macbook air',
+                            category='computers/laptops',
+                            price=1499.0)))
+
+    def get_all_products(self):
+        """
+        send a GET request to fetch all products
+        """
+        return self.client.get(
+             '/api/v1/products', content_type='application/json')
