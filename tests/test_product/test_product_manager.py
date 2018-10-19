d8d8ad311a08f5f818344439b2a3028a1f85b60e
myco <coditmyco6@gmail.com>
2018-10-19 20:52:30 +0300

    [Finishes #161335327] write tests for the product class

---
+++ tests/test_product/test_product_manager.py
@@ -0,0 +1,16 @@
+from tests.fixture import FixtureTest
+from flasky.product.manager import product_manager
+
+
+class TestProductManager(FixtureTest):
+    """
+    Test class for testing product manager class
+    """
+
+
+    def test_product_manager_init(self):
+        self.assertIsInstance(product_manager.products, dict)
+
+    def test_add_new_product_increases_products_count(self):
+        product_manager.add_product(self.product)
+        self.assertEqual(len(product_manager.products), 1)
