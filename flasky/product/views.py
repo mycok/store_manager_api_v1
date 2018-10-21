5cc20c4211b401c84237cf61620e3e2f0fc18dcf
myco <coditmyco6@gmail.com>
2018-10-21 22:14:03 +0300

    [Fixes #161363224] Refactor to remove unused imports

--- flasky/product/views.py
+++ flasky/product/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request, url_for
+from flask import Blueprint, request
 from flask.views import MethodView
 from flasky.product.model import Product
 from flasky.product.input_validation import is_product_input_valid
