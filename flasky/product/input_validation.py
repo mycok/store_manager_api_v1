7fd83b9070b90b2df161484e5527f556772c6462
myco <coditmyco6@gmail.com>
2018-10-25 18:01:22 +0300

    Add validation for shopping_cart model

--- flasky/product/input_validation.py
+++ flasky/product/input_validation.py
@@ -1,5 +1,9 @@
 def is_arg_valid(arg=None):
-    return arg is not None and arg != '' and not arg.isspace()
+    return arg is not None and is_string_arg(arg) and arg != '' and not arg.isspace()
+
+
+def is_string_arg(arg):
+    return isinstance(arg, str) and not arg.isdigit()
 
 
 def is_numb_valid(numb=None):
@@ -12,15 +16,16 @@ def is_currency_valid(currency=None):
             currency, float)
 
 
-def is_product_input_valid(name, category, quantity, price):
+def is_product_input_valid(name=None, category=None,
+                           quantity=None, price=None):
     message = None
     if not is_arg_valid(arg=name):
         message = 'please provide a valid product name'
     elif not is_arg_valid(arg=category):
         message = 'please provide a valid product category'
-    elif not is_numb_valid(quantity):
+    elif not is_numb_valid(numb=quantity):
         message = 'please provide a valid product quantity'
-    elif not is_currency_valid(price):
+    elif not is_currency_valid(currency=price):
         message = 'please provide a valid product price'
 
     if message is not None:
@@ -31,4 +36,12 @@ def is_product_input_valid(name, category, quantity, price):
 def is_sale_input_valid(attendant):
     if not is_arg_valid(arg=attendant):
         return 'please provide a valid attendant name'
+    return True
+
+
+def is_valid_shopping_cart_product(name=None, quantity=None):
+    if not is_arg_valid(arg=name):
+        return 'please provide a valid product name'
+    elif not is_numb_valid(numb=quantity):
+        return 'please provide a valid product quantity'
     return True
