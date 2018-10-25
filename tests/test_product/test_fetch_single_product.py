fef1273f3ab14d747252f732186e79171cce9b6a
myco <coditmyco6@gmail.com>
2018-10-25 19:47:19 +0300

    Refacor more tests

--- tests/test_product/test_fetch_single_product.py
+++ tests/test_product/test_fetch_single_product.py
@@ -4,25 +4,6 @@ from tests.fixture import FixtureTest
 
 
 class TestFetchSingleProduct(FixtureTest):
-
-    # def test_get_product_by_id(self):
-    #     # Test successful GET request to fetch a product by id
-    #     with self.client:
-    #         # post a product
-    #         post_response = self.create_product()
-    #         post_data = json.loads(post_response.data.decode())
-    #         product_id = post_data.get('product_id')
-    #         self.assertIsInstance(product_id, int)
-
-    #         # get product by id
-    #         response = self.client.get(
-    #             '/api/v1/products/product_id',
-    #             content_type='application/json'
-    #         )
-    #         data = json.loads(response.data.decode())
-    #         self.assertEqual(response.status_code, 200)
-    #         self.assertTrue(data['name'], 'macbook pro')
-
     def test_cant_fetch_product_with_out_of_range_index(self):
         """
         Test unsuccessful GET request to fetch
