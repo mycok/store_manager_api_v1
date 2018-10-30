026c6d9d92e9ac39ecf9065877e132fb371af945
myco <coditmyco6@gmail.com>
2018-10-30 10:59:13 +0300

    Change database configurations

--- flasky/auth/user_model_controller.py
+++ flasky/auth/user_model_controller.py
@@ -1,29 +1,25 @@
-from flasky.database.postgres import DataBase as db
+from flasky.database.postgres import db
 
 
-class Controller(object):
-    """
-    Controller class for handling and storing user data
-    """
-    def __init__(self):
-        self.db = db
-        self.db.connect('rides', 'Myko', '1987')
-        self.db.create_db_tables()
+class UserController:
 
-    def create_user(self, user):
+    # Controller class for handling and storing user data
+    @classmethod
+    def create_user(cls, user):
         values = (user.user_id, user.username,
                   user.role, user.email, user.password_hash)
         query = "INSERT INTO users (user_id, username, role, email, password) VALUES\
-        ({}, {}, {}, {}, {})".format(*values)
-        self.db.insert(query, values)
+        (%s, %s, %s, %s, %s)"
+        values = (user.user_id, user.username,
+                  user.role, user.email, user.password_hash)
+        db.insert(query, values)
 
-    def check_if_user_exists(self, email):
+    @classmethod
+    def check_if_user_exists(cls, email):
         query = "SELECT * FROM users WHERE email = '{}'".format(email)
-        return self.db.check(query)
+        return db.check(query)
 
-    def fetch_user_by_id(self, user_id):
+    @classmethod
+    def fetch_user_by_id(cls, user_id):
         query = "SELECT * FROM users WHERE user_id = '{}'".format(user_id)
-        return self.db.check(query)
-
-# A user controller instance
-controller = Controller()
+        return db.check(query)
