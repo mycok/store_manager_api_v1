074f83fa592aae4db8f8c2aa7bd15bc1bd4cbeb9
myco <coditmyco6@gmail.com>
2018-10-30 10:55:11 +0300

    Add database intergration to app factory

--- flasky/__init__.py
+++ flasky/__init__.py
@@ -5,7 +5,7 @@ from flasky.product.product_view import products_bp
 from flasky.sale.sale_view import sales_bp
 from flasky.shopping_cart.shopping_cart_view import shopping_cart_bp
 from flasky.auth.views import auth_bp
-from flasky.database.postgres import DataBase as DB
+from flasky.database.postgres import db
 
 
 def create_app(config_name=None):
@@ -20,5 +20,8 @@ def create_app(config_name=None):
     app.register_blueprint(sales_bp)
     app.register_blueprint(shopping_cart_bp)
     app.register_blueprint(auth_bp)
+    # database setup
+    db.connect('rides', 'Myko', '1987')
+    db.create_db_tables()
 
     return app
