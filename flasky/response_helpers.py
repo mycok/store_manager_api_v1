51af8e09d1dc6618cd4b3868052815dc25d66658
myco <coditmyco6@gmail.com>
2018-10-30 11:17:21 +0300

    Remove unused response methods

--- flasky/response_helpers.py
+++ flasky/response_helpers.py
@@ -10,7 +10,7 @@ def response(message, status, status_code):
 
 
 # product custom responses
-def single_product_response(product, status, status_code):
+def single_product_response(product):
     return make_response(jsonify({
         'product_id': product.product_id,
         'name': product.name,
@@ -18,8 +18,24 @@ def single_product_response(product, status, status_code):
         'price': product.price,
         'in_stock': product.quantity,
         'sales': product.sales,
-        'status': status
-    })), status_code
+        'status': 'success'
+    })), 200
+
+
+def create_resource_response(resource, resource_url):
+    return make_response(jsonify({
+        'message': '{} has been added'.format(resource.name),
+        'status': 'successful',
+        'product_id': resource.product_id,
+        'resource_link': resource_url
+    })), 201
+
+
+def fetch_all_response(resource, resource_name):
+    return make_response(jsonify({
+        '{}'.format(resource_name): resource,
+        'status': 'successful'
+    })), 200
 
 
 def all_products_response(products, status, status_code):
@@ -30,15 +46,23 @@ def all_products_response(products, status, status_code):
 
 
 # sale custom responses
-def single_sale_response(sale, status, status_code):
+def single_sale_response(sale, status_code):
     return make_response(jsonify({
         'sale_id': sale.sale_id,
         'attendant': sale.attendant,
         'products': sale.products,
         'total_products': sale.total_products,
         'total_amount': sale.total_amount,
-        'status': status
+        'status': 'success'
     })), status_code
+
+
+def create_sale_response(sale, resource_url):
+    return make_response(jsonify({
+        'message': 'sale with {} has been added'.format(sale.sale_id),
+        'status': 'successful',
+        'resource_link': resource_url
+    })), 201
 
 
 def all_sales_response(sales, status, status_code):
