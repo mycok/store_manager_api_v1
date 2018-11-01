f73d1668966534603868868a9abfa631c177a786
myco <coditmyco6@gmail.com>
2018-11-01 11:12:49 +0300

    [Finishes

--- tests/test_product/test_create_single_product.py
+++ tests/test_product/test_create_single_product.py
@@ -15,13 +15,12 @@ class TestCreateProduct(TestFixture):
     def test_cant_create_a_duplicate_product(self):
         # Test successful POST request to create a product
         with self.client:
-            response = self.create_product()
-            # self.assertEqual(response.status_code, 201)
+            _ = self.create_product()
 
             new_response = self.create_product()
             data = json.loads(new_response.data.decode())
             self.assertEqual(new_response.status_code, 400)
-            self.assertEqual(data['message'], 'Product macbookair already exists')
+            self.assertEqual(data['message'], 'product macbookair already exists')
 
     def test_create_product_with_wrong_content_type(self):
         """
@@ -29,12 +28,7 @@ class TestCreateProduct(TestFixture):
         create a product with wrong content type
         """
         with self.client:
-            response = self.client.post(
-                '/api/v1/products',
-                content_type="xml",
-                data=json.dumps(dict(name='macbook air',
-                                category='computers/laptops',
-                                quantity=4, price=1499.0)))
+            response = self.create_product_with_wrong_content_type()
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 400)
             self.assertEqual(data['message'], 'request must be of type json')
@@ -53,18 +47,14 @@ class TestCreateProduct(TestFixture):
             self.assertEqual(data['message'], 'quantity should be a number greater than zero')
             self.assertEqual(data['status'], 'unsuccessful')
 
-    def test_cant_create_product_with_an_empty_string(self):
+    def test_cant_create_product_with_an_empty_string_as_name(self):
         """
          Test unsuccessful POST request to
         create a product with an empty string
         """
         with self.client:
-            response = self.client.post(
-                '/api/v1/products',
-                content_type="application/json",
-                data=json.dumps(dict(name='  ',
-                                category='computers/laptops',
-                                quantity=4, price=1499.0)))
+            response = self.create_product_with_an_empty_string()
+
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 400)
             self.assertEqual(
