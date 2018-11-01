7240984828dae896ac8daa5aa23e1289396e3c32
myco <coditmyco6@gmail.com>
2018-11-01 12:50:10 +0300

    Refactor to remove sale and shopping cart blueprint imports

--- flasky/__init__.py
+++ flasky/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 
 from flasky.config import DevelopmentConfig
 from flasky.product.product_view import products_bp
-from flasky.sale.sale_view import sales_bp
-from flasky.shopping_cart.shopping_cart_view import shopping_cart_bp
 from flasky.auth.views import auth_bp
 from flasky.database.postgres import db
 
@@ -21,8 +19,6 @@ def create_app(config_name=None):
         db.create_db_tables()
     # register blueprints
     app.register_blueprint(products_bp)
-    app.register_blueprint(sales_bp)
-    app.register_blueprint(shopping_cart_bp)
     app.register_blueprint(auth_bp)
 
     return app
