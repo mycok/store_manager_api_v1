723f4726eb29f519e8f28ba1443e1999141b1300
myco <coditmyco6@gmail.com>
2018-11-01 11:07:11 +0300

    [Finishes #161553616] Admin can modify product

--- flasky/product/product_view.py
+++ flasky/product/product_view.py
@@ -4,16 +4,18 @@ from flask.views import MethodView
 
 from flasky.product.product_model import Product
 from flasky.validator import Validation as v
-from flasky.product.product_controller import controller
+from flasky.product.product_controller import Controller
 from flasky.response_helpers import (
     create_resource_response, response,
     single_product_response, fetch_all_response,
     convert_list_to_json
 )
-from flasky.auth.auth_decorator import token_required
+from flasky.auth.auth_decorator import (
+    token_required, is_admin_role, is_attendant_role
+)
 
 
-products_bp = Blueprint('products', __name__, url_prefix='/api/v1')
+products_bp = Blueprint('products', __name__, url_prefix='/api/v2')
 
 
 class ProductListView(MethodView):
@@ -23,12 +25,18 @@ class ProductListView(MethodView):
 
     methods = ['POST', 'GET']
 
+    decorators = [token_required]
+
     # create a product
-    def post(self):
+    @classmethod
+    def post(cls, current_user):
         # check for a valid content type
         if not request.content_type == 'application/json':
             return response('request must be of type json',
                             'unsuccessful', 400)
+        if not is_admin_role(current_user.role):
+            return response('Admin previllages required', 'unsuccessful', 401)
+
         # extract request data
         request_data = request.get_json()
         name = request_data.get('name')
@@ -36,29 +44,29 @@ class ProductListView(MethodView):
         quantity = request_data.get('quantity')
         price = request_data.get('price')
         # validate product object input
-        valid_input = v.validate_product(name, category, quantity, price)
+        valid_input = v.validate_product(name=name, category=category, quantity=quantity, price=price)
         if not isinstance(valid_input, bool):
             return response(valid_input, 'unsuccessful', 400)
         # create a new product
         new_product = Product(name=name, category=category,
                               quantity=quantity, price=price)
         # check if a product with the same name already exists
-        is_existing_product = controller.add_product(new_product)
-        if not isinstance(is_existing_product, bool):
-            return response(is_existing_product, 'unsuccessful', 400)
+        existing_product = Controller.add_product(new_product)
+        if existing_product is not None:
+            return response(existing_product, 'unsuccessful', 400)
         # return response with a new product
         return create_resource_response(
             new_product,
             url_for('products.product', product_id=new_product.product_id))
 
     # fetch all products
-    def get(self):
+    @classmethod
+    def get(cls, current_user):
 
-        products = controller.fetch_all_products()
+        products = Controller.fetch_all_products()
         if isinstance(products, str):
             return response(products, 'unsuccessful', 200)
-        return fetch_all_response(
-            convert_list_to_json(products), 'products')
+        return fetch_all_response(products, 'products')
 
 
 class ProductView(MethodView):
@@ -67,14 +75,68 @@ class ProductView(MethodView):
     /products/<int:product_id> endpoint
     """
 
-    methods = ['GET']
+    methods = ['GET', 'PUT', 'DELETE']
 
-    def get(self, product_id):
+    decorators = [token_required]
+
+    @classmethod
+    def get(cls, current_user, product_id):
         # GET request to fetch a product by id
-        product = controller.fetch_product_by_id(product_id)
-        if not isinstance(product, Product):
+        product = Controller.fetch_product_by_id(product_id)
+        if not isinstance(product, dict):
             return response(product, 'unsuccessful', 400)
-        return single_product_response(product)
+        return response(product, 'successful', 200)
+
+    # PUT
+    @classmethod
+    def put(cls, current_user, product_id):
+        """
+        PUT request to update contents
+        of a product by id
+
+        Arguments:
+            PRODUCT_id --int-- A unique integer id assigned to a single product
+        """
+        if not request.content_type == 'application/json':
+            return response('request must be of type json', 'failed', 400)
+        if not is_admin_role(current_user.role):
+            return response('Admin previllages required', 'unsuccessful', 401)
+
+        sent_data = request.get_json()
+        name = sent_data.get('name')
+        category = sent_data.get('category')
+        quantity = sent_data.get('quantity')
+        price = sent_data.get('price')
+
+        # query the database through a manager object
+        updated = Controller.update_product(
+            name, category, quantity, price, product_id)
+        if updated is None:
+            return response(updated, 'unsuccessful', 400)
+        return response(str(updated) + ' row successfully updated', 'success', 200)
+
+    # DELETE
+    @classmethod
+    def delete(cls, current_user, product_id):
+        """
+        DELETE request to delete a product by id
+
+        Arguments:
+            product_id --int-- A unique integer id assigned to a single product
+        """
+        if not request.content_type == 'application/json':
+            return response('request must be of type json', 'failed', 400)
+        if not is_admin_role(current_user.role):
+            return response('Admin previllages required', 'unsuccessful', 401)
+
+        # delete the product
+        deleted = Controller.delete_product_by_id(product_id)
+        if deleted is not None:
+            return response(
+                'product with ID {0} has been deleted'.format(product_id),
+                'successful', 200)
+        # otherwise return a string response
+        return response("product doesnot exist", "unsuccessful", 400)
 
 
 # Register a class as a view
