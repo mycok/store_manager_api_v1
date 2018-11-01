efd51a9369711029c79a6c116a40acf8f3963e02
myco <coditmyco6@gmail.com>
2018-11-01 11:03:26 +0300

    [Finishes #161573359] Add user signup_login_logout

--- flasky/auth/views.py
+++ flasky/auth/views.py
@@ -11,39 +11,50 @@ from flasky.auth.invalid_token_controller import TokenController
 from flasky.auth.user_model import User
 from flasky.auth.invalid_token_model import InvalidToken
 from flasky.validator import Validation as v
+from flasky.auth.auth_decorator import (
+    token_required, is_admin_role, is_attendant_role
+)
 
 
-auth_bp = Blueprint('auth', __name__, url_prefix='/api/v1')
+auth_bp = Blueprint('auth', __name__, url_prefix='/api/v2')
 
 
 class SignUp(MethodView):
 
     methods = ['POST']
 
+    decorators = [token_required]
+
     @classmethod
-    def post(cls):
+    def post(cls, current_user):
+        # check request content type
         if not request.content_type == 'application/json':
             abort(400)
-
+        # check user role
+        if not is_admin_role(current_user.role):
+            return response('Admin previllages required', 'unsuccessful', 401)
+        # extract request data
         sent_data = request.get_json()
         username = sent_data.get('username')
         role = sent_data.get('role')
         email = sent_data.get('email')
         password = sent_data.get('password')
-
+        #validate input
         is_input_valid = v.validate_user(username, email, password)
         if not isinstance(is_input_valid, bool):
             return response(is_input_valid, 'unsuccessful', 400)
-        # Create a new user
+        # Create a new user from the provided input
         new_user = User(username=username,
                         role=role, email=email, password=password)
         # Check the new_user already exists
         existing_user = controller.check_if_user_exists(email)
         if existing_user is not None:
-            return response('user with email ' + existing_user['email'] + ' already exists', 'unsuccessful', 400)
-
+            return response(
+                'user with email {} already exists'.format(existing_user['email']),
+                'unsuccessful', 400)
+        # save new user to the database
         controller.create_user(new_user)
-
+        # return a success response
         return create_user_response(
             new_user, url_for('auth.register', email=new_user.email))
 
@@ -54,31 +65,32 @@ class Login(MethodView):
 
     @classmethod
     def post(cls):
-
+        # check request content type
         if not request.content_type == 'application/json':
             abort(400)
-
+        # extract request data
         sent_data = request.get_json()
         email = sent_data.get('email')
         password = sent_data.get('password')
-
+        #validate input
         is_input_valid = v.validate_user_login(email=email, password=password)
         if not isinstance(is_input_valid, bool):
             return response(is_input_valid, 'unsuccessful', 401)
-
+        # Check the user already exists
         existing_user = controller.check_if_user_exists(email)
         if existing_user is None:
             return response(
                 'user with email ' + email + ' doesnot exist',
                 'unsuccessful', 401)
-
+        # verify password
         if not User.check_password(existing_user['password'], password):
             return response(
                 'provided password is incorrect', 'unsuccessful', 401)
-
+        # generate jwt token
         token = User.generate_auth_token(existing_user['user_id'])
         if not isinstance(token, bytes):
             return response(str(token), 'unsuccessful', 401)
+        # return a success response
         return auth_success_response(
            existing_user['username'] + ' is now logged in', token)
 
@@ -89,29 +101,33 @@ class Logout(MethodView):
 
     @classmethod
     def post(cls):
-
+        # check for token in request header
         auth_header = request.headers.get('Authorization')
         if not auth_header:
             return response(
                 'Provide an authorization header', 'unsuccessful', 403)
         try:
+            # extract token from request header
             auth_token = auth_header.split(" ")[1]
 
         except IndexError:
             return response('unable to extract token', 'unsuccessful', 403)
         else:
+            # decode token to retrieve a user_id
             decoded_token_response = User.decode_auth_token(auth_token)
 
-            if not isinstance(decoded_token_response, int):
+            if isinstance(decoded_token_response, int):
                 return response(decoded_token_response, 'unsuccessful', 401)
+        # check if the token is already blacklisted
             is_token_invalid = TokenController.check_if_token_exists(
                 auth_token)
 
-            if isinstance(is_token_invalid, list):
+            if isinstance(is_token_invalid, dict):
                 return response(
                     'token already invalidated', 'unsuccessful', 400)
+            # blacklist/invalidate token
             token = InvalidToken(auth_token)
-
+            # save blacklisted token to a databse
             TokenController.save_invalid_token(token)
             return response('successfully logged out', 'success', 200)
 
