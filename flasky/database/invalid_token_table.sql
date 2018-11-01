9dfdb8bba77af2e4e083cc70205c8607a34bcbba
myco <coditmyco6@gmail.com>
2018-11-01 10:47:16 +0300

    [Finishes #161642837] Refactor to isolate sql statements

---
+++ flasky/database/invalid_token_table.sql
@@ -0,0 +1,5 @@
+
+CREATE TABLE IF NOT EXISTS invalidtokens (
+  token_id SERIAL PRIMARY KEY,
+  token VARCHAR(255) DEFAULT NULL
+);
---
+++ flasky/database/users_table.sql
@@ -0,0 +1,14 @@
+-- CREATE TYPE DESCP AS ENUM ('Admin', 'Attendant');
+
+
+CREATE TABLE IF NOT EXISTS users (
+  user_id TEXT NOT NULL PRIMARY KEY,
+  username VARCHAR(255) DEFAULT NULL,
+  email VARCHAR(255) DEFAULT NULL,
+  password VARCHAR(255) DEFAULT NULL,
+  role DESCP DEFAULT 'Admin',
+  created_timestamp TIMESTAMP DEFAULT NOW()
+);
+
+-- INSERT INTO users (user_id, username, email, password) VALUES(23489657345, 'kibuuka', 'me2@mail.com', '$2a$12$IQR112ysyLTUO1RfJWzkUejOLpJzz7xlGG.PykpYn9k72hnROEf.O
+-- ');
