258db302fe218b9f868977c0dc3eae476774f01b
myco <coditmyco6@gmail.com>
2018-11-05 13:10:31 +0300

    Add test methods for product and sale objects

--- tests/fixture.py
+++ tests/fixture.py
@@ -5,6 +5,7 @@ from flasky import create_app
 from flasky.config import TestingConfig
 from flasky.product.product_model import Product
 from flasky.auth.user_model import User
+from flasky.sale.sale_model import Sale
 from flasky.auth.user_model_controller import UserController
 from flasky.product.product_controller import Controller
 from flasky.database.postgres import DataBase
@@ -22,6 +23,7 @@ class TestFixture(TestCase):
         self.db.create_db_tables()
         self.client = self.app.test_client()
         self.product = Product('macbook', 'computers/laptops', 3, 1499.0)
+        self.sale = Sale('prodigal')
 
     def tearDown(self):
         self.db.drop_tables()
@@ -49,10 +51,30 @@ class TestFixture(TestCase):
                 (dict(username=name, role=role, email=email, password=password)))
         )
 
+    def attendant_login(self):
+        email = 'aweso@try.net'
+        password = '92&Va^rtyu4'
+        self.direct_register_attendant()
+        return self.client.post(
+            '/api/v2/auth/login', content_type='application/json',
+            data=json.dumps((dict(email=email, password=password))))
+
     def create_attendants_with_invalid_input(self, token):
         name = 'intent'
         role = 'Attendant'
         email = 'smthtry.com'
+        password = 'tHis#1245f'
+        return self.client.post(
+            '/api/v2/auth/signup', content_type='application/json',
+            headers=dict(Authorization='Bearer ' + token),
+            data=json.dumps(
+                (dict(username=name, role=role, email=email, password=password)))
+        )
+
+    def cant_create_attendants_with_invalid_name(self, token):
+        name = ' '
+        role = 'Attendant'
+        email = 'smth@try.com'
         password = 'tHis#1245f'
         return self.client.post(
             '/api/v2/auth/signup', content_type='application/json',
@@ -97,6 +119,24 @@ class TestFixture(TestCase):
             '/api/v2/auth/login', content_type='application/json',
             data=json.dumps((dict(email=email, password=password))))
 
+    def invalid_email_user_login_(self):
+        email = 'me2mail.com'
+        password = 'dNa#$1236'
+
+        self.direct_register_admin()
+        return self.client.post(
+            '/api/v2/auth/login', content_type='application/json',
+            data=json.dumps((dict(email=email, password=password))))
+
+    def missing_password_user_login_(self):
+        email = 'me2mail@mail.com'
+        password = ''
+
+        self.direct_register_admin()
+        return self.client.post(
+            '/api/v2/auth/login', content_type='application/json',
+            data=json.dumps((dict(email=email, password=password))))
+
     def user_logout(self):
         response = self.user_login()
         data = json.loads(response.data.decode())
@@ -131,6 +171,21 @@ class TestFixture(TestCase):
         send a POST request to create a product object
         """
         response = self.user_login()
+        data = json.loads(response.data.decode())
+        token = data['token']
+
+        return self.client.post(
+            '/api/v2/products', content_type='application/json',
+            headers=dict(Authorization='Bearer ' + token),
+            data=json.dumps(dict(name='macbookair',
+                                 category='computers',
+                                 quantity=4, price=1499.0)))
+
+    def cant_create_product_as_an_attendant(self):
+        """
+        send a POST request to create a product object
+        """
+        response = self.attendant_login()
         data = json.loads(response.data.decode())
         token = data['token']
 
@@ -249,6 +304,42 @@ class TestFixture(TestCase):
                                  category='computers',
                                  quantity=70, price=1499.0)))
 
+    def cant_update_product_as_an_attendant(self):
+        response = self.attendant_login()
+        data = json.loads(response.data.decode())
+        token = data['token']
+        # post a product
+        product_response = self.create_product()
+        data = json.loads(product_response.data.decode())
+        product_id = data['product_id']
+
+        # get that product by id
+        return self.client.put(
+            '/api/v2/products/{}'.format(product_id),
+            content_type='application/json',
+            headers=dict(Authorization='Bearer ' + token),
+            data=json.dumps(dict(name='macbookair',
+                                 category='computers',
+                                 quantity=70, price=1499.0)))
+
+    def cant_update_product_with_invalid_content_type(self):
+        response = self.user_login()
+        data = json.loads(response.data.decode())
+        token = data['token']
+        # post a product
+        product_response = self.create_product()
+        data = json.loads(product_response.data.decode())
+        product_id = data['product_id']
+
+        # get that product by id
+        return self.client.put(
+            '/api/v2/products/{}'.format(product_id),
+            content_type='xml',
+            headers=dict(Authorization='Bearer ' + token),
+            data=json.dumps(dict(name='macbookair',
+                                 category='computers',
+                                 quantity=70, price=1499.0)))
+
     def delete_product(self):
         response = self.user_login()
         data = json.loads(response.data.decode())
@@ -269,8 +360,41 @@ class TestFixture(TestCase):
         """
         send a POST request to create a sale object
         """
+        response = self.attendant_login()
+        data = json.loads(response.data.decode())
+        token = data['token']
+
         return self.client.post(
             '/api/v2/sales', content_type='application/json',
+            headers=dict(Authorization='Bearer ' + token),
+            data=json.dumps(
+                dict(attendant='michael')))
+
+    def create_sale_as_admin(self):
+        """
+        send a POST request to create a sale object as an admin
+        """
+        response = self.user_login()
+        data = json.loads(response.data.decode())
+        token = data['token']
+
+        return self.client.post(
+            '/api/v2/sales', content_type='application/json',
+            headers=dict(Authorization='Bearer ' + token),
+            data=json.dumps(
+                dict(attendant='michael')))
+
+    def create_sale_with_invalid_previllages(self):
+        """
+        send a POST request to create a sale object
+        """
+        response = self.user_login()
+        data = json.loads(response.data.decode())
+        token = data['token']
+
+        return self.client.post(
+            '/api/v2/sales', content_type='application/json',
+            headers=dict(Authorization='Bearer ' + token),
             data=json.dumps(
                 dict(attendant='michael')))
 
@@ -279,19 +403,59 @@ class TestFixture(TestCase):
         send a POST request to create a sale object
         with  a missing required parameter.
         """
+        response = self.attendant_login()
+        data = json.loads(response.data.decode())
+        token = data['token']
+
         return self.client.post(
             '/api/v2/sales', content_type='application/json',
+            headers=dict(Authorization='Bearer ' + token),
             data=json.dumps(dict(attendant='  ')))
 
+    def create_sale_with_wrong_content_type(self):
+        response = self.attendant_login()
+        data = json.loads(response.data.decode())
+        token = data['token']
+
+        return self.client.post(
+                '/api/v2/sales',
+                content_type="xml",
+                headers=dict(Authorization='Bearer ' + token),
+                data=json.dumps(dict(attendant='ivan')))
+
     def get_all_sales(self):
+        """
+        send a GET request to fetch all sales
+        """
+        response = self.user_login()
+        data = json.loads(response.data.decode())
+        token = data['token']
+
+        return self.client.get(
+            '/api/v2/sales', content_type='application/json',
+            headers=dict(Authorization='Bearer ' + token))
+
+    def get_all_sales_without_a_token(self):
         """
         send a GET request to fetch all sales
         """
         return self.client.get(
             '/api/v2/sales', content_type='application/json')
 
+    def get_all_sales_as_an_attendant(self):
+        """
+        send a GET request to fetch all sales
+        """
+        response = self.attendant_login()
+        data = json.loads(response.data.decode())
+        token = data['token']
+
+        return self.client.get(
+            '/api/v2/sales', content_type='application/json',
+            headers=dict(Authorization='Bearer ' + token))
+
 # shopping cart helper methods
-    def add_product_to_shopping_cart(self):
+    def add_product_to_cart(self):
         """
         send a POST request to add a product to cart
         """
