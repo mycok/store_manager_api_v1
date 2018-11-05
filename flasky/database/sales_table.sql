093fbddb5751c23a2e1ba1ed53c7f6bf3d796cec
myco <coditmyco6@gmail.com>
2018-11-05 13:07:46 +0300

    Change products column type to text(array)

--- flasky/database/sales_table.sql
+++ flasky/database/sales_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS sales (
   sale_id TEXT NOT NULL PRIMARY KEY,
   attendant VARCHAR(255) DEFAULT NULL,
-  products JSONB,
+  products TEXT ARRAY,
   total_product VARCHAR(255) DEFAULT NULL,
   total_amount VARCHAR(255) DEFAULT NULL,
   created_timestamp TIMESTAMP DEFAULT NOW()
