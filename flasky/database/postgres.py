ee646267a88338139ac3e7cb67100c226c33823a
myco <coditmyco6@gmail.com>
2018-11-10 08:38:39 +0300

    Refactor database init method

--- flasky/database/postgres.py
+++ flasky/database/postgres.py
@@ -19,75 +19,55 @@ class DataBase:
     """
 
     def __init__(self):
-        try:
-            self.connection = psycopg2.connect(
-                user='yerumrcmmbodcj',
-                password='a1b8aa59b3efb84c23ab7ac94f479c2592dbd1c23658e4a0e01975b0c336e80d',
-                database='d3du2vcd5d0031',
-                port='5432')
+        pass
 
-            self.cursor = self.connection.cursor(
+    @classmethod
+    def connect(cls, **kwargs):
+        try:
+            cls.connection = psycopg2.connect(
+                user=kwargs['user'],
+                database=kwargs['database'],
+                password=kwargs['password']
+                )
+
+            cls.cursor = cls.connection.cursor(
                 cursor_factory=psycopg2.extras.RealDictCursor)
             print("...connected....")
 
         except (Exception) as error:
-            self.connection.rollback()
-            self.connection.commit()
+            # cls.connection.rollback()
+            # cls.connection.commit()
             print('Failed to connect to the database {}'.format(error))
 
-    # @classmethod
-    # def connect(cls, **kwargs):
-        # try:
-        #     cls.connection = psycopg2.connect(
-        #         Host=kwargs['host'], Database=kwargs['database'],
-        #         User=kwargs['user'],
-        #         Password=kwargs['password'], port=kwargs['port'])
+    @classmethod
+    def create_db_tables(cls):
+        # create database tables
+        create_tables_sql = ('flasky/database/invalid_token_table.sql',
+                             'flasky/database/users_table.sql',
+                             'flasky/database/products_table.sql',
+                             'flasky/database/cart_table.sql',
+                             'flasky/database/sales_table.sql')
 
-        #     cls.cursor = cls.connection.cursor(
-        #         cursor_factory=psycopg2.extras.RealDictCursor)
-        #     print("...connected....")
+        for table in create_tables_sql:
+            query = open(table, mode='r', encoding='utf-8').read()
+            cls.cursor.execute(query)
+            cls.connection.commit()
 
-        # except (Exception) as error:
-        #     # cls.connection.rollback()
-        #     # cls.connection.commit()
-        #     print('Failed to connect to the database {}'.format(error))
+    @classmethod
+    def drop_tables(cls):
+        # drop/delete all existing tables from the database
+        drop_tables_sql = (
+            'DROP TABLE IF EXISTS users CASCADE',
+            'DROP TABLE IF EXISTS products CASCADE',
+            'DROP TABLE IF EXISTS invalidtokens CASCADE',
+            'DROP TABLE IF EXISTS cart CASCADE',
+            'DROP TABLE IF EXISTS sales CASCADE'
+        )
 
-    def create_db_tables(self):
-        token_file = "flasky/database/invalid_token_table.sql"
-        users_file = "flasky/database/users_table.sql"
-        product_file = "flasky/database/products_table.sql"
-        cart_file = "flasky/database/cart_table.sql"
-        sale_file = "flasky/database/sales_table.sql"
-
-        token_sql = open(token_file, mode='r', encoding='utf-8').read()
-        users_sql = open(users_file, mode='r', encoding='utf-8').read()
-        product_sql = open(product_file, mode='r', encoding='utf-8').read()
-        cart_sql = open(cart_file, mode='r', encoding='utf-8').read()
-        sales_sql = open(sale_file, mode='r', encoding='utf-8').read()
-
-        self.cursor.execute(users_sql)
-        self.cursor.execute(product_sql)
-        self.cursor.execute(token_sql)
-        self.cursor.execute(cart_sql)
-        self.cursor.execute(sales_sql)
-
-        self.connection.commit()
-
-    def drop_tables(self):
-        users = "DROP TABLE IF EXISTS users CASCADE"
-        products = "DROP TABLE IF EXISTS products CASCADE"
-        invalidtokens = "DROP TABLE IF EXISTS invalidtokens CASCADE"
-        cart = "DROP TABLE IF EXISTS cart_table CASCADE"
-        sales = "DROP TABLE IF EXISTS sales CASCADE"
-
-        self.cursor.execute(users)
-        self.cursor.execute(products)
-        self.cursor.execute(invalidtokens)
-        self.cursor.execute(cart)
-        self.cursor.execute(sales)
-
-        self.connection.commit()
-        print('...dropped...')
+        for table_query in drop_tables_sql:
+            cls.cursor.execute(table_query)
+            cls.connection.commit()
+            print('...dropped...')
 
     def select_query(self, query):
         items = self.fetch_all(query)
