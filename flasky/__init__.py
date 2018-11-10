a4dad05ac6cb771c3f36b502bd89754725450e08
myco <coditmyco6@gmail.com>
2018-11-10 08:02:41 +0300

    Add heroku config

--- flasky/__init__.py
+++ flasky/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from flasky.config import ProductionConfig
+from flasky.config import DevelopmentConfig
 from flasky.product.product_view import products_bp
 from flasky.auth.views import auth_bp
 from flasky.database.postgres import db
@@ -15,15 +15,13 @@ def create_app(config_name=None):
     if config_name is not None:
         app.config.from_object(config_name)
     else:
-        app.config.from_object(ProductionConfig)
+        app.config.from_object(DevelopmentConfig)
         # database setup
-        # db.connect(
-        #     host='ec2-107-22-241-243.compute-1.amazonaws.com',
-        #     database='d3du2vcd5d0031',
-        #     user='yerumrcmmbodcj',
-        #     password='a1b8aa59b3efb84c23ab7ac94f479c2592dbd1c23658e4a0e01975b0c336e80d',
-        #     port='5432'
-        #     )
+        db.connect(
+            user='Myko',
+            database='my_store',
+            password=1987
+            )
         db.create_db_tables()
     # register blueprints
     app.register_blueprint(products_bp)
