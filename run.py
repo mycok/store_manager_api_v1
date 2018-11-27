e7267e36cfd5e5d0ed97d6b5cbe733ceb2aacd47
myco <coditmyco6@gmail.com>
2018-11-27 16:05:27 +0300

    Add an index route

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
+from flask import render_template
 from flasky import create_app
+from flasky.config import ProductionConfig
 
 
 app = create_app()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
