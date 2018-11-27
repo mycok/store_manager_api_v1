8452fe716f646bc917c7561cc0cde29c4c70cd48
myco <coditmyco6@gmail.com>
2018-11-27 15:49:55 +0300

    Change database connection parameters

--- flasky/__init__.py
+++ flasky/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from flask_cors import CORS
 
-from flasky.config import DevelopmentConfig
+from flasky.config import ProductionConfig
 from flasky.product.product_view import products_bp
 from flasky.auth.views import auth_bp
 from flasky.database.postgres import db
@@ -11,20 +12,22 @@ from flasky.cart.cart_view import cart_bp
 def create_app(config_name=None):
 
     app = Flask(__name__)
+    CORS(app)
     # configure app
     if config_name is not None:
         app.config.from_object(config_name)
     else:
-        app.config.from_object(DevelopmentConfig)
+        app.config.from_object(ProductionConfig)
         # database setup
         db.connect(
-            host='ec2-107-22-241-243.compute-1.amazonaws.com',
-            database='d3du2vcd5d0031',
-            user='yerumrcmmbodcj',
-            password='a1b8aa59b3efb84c23ab7ac94f479c2592dbd1c23658e4a0e01975b0c336e80d',
-            port='5432'
+            host=ProductionConfig.HOST,
+            database=ProductionConfig.DATABASE,
+            user=ProductionConfig.USER,
+            password=ProductionConfig.PASSWORD,
+            port=ProductionConfig.PORT
             )
         db.create_db_tables()
+
     # register blueprints
     app.register_blueprint(products_bp)
     app.register_blueprint(auth_bp)
