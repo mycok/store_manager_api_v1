b9ecc9f768cdf42c75aed2cc6f026d0ba95e8fe1
myco <coditmyco6@gmail.com>
2018-11-27 15:56:31 +0300

    Change string formating

--- flasky/product/product_controller.py
+++ flasky/product/product_controller.py
@@ -12,32 +12,39 @@ class Controller:
         is_existing = cls.check_if_product_exists(product.name)
         if is_existing is None:
             product.product_id = generate_id()
-            query = "INSERT INTO products (product_id, name, category, quantity, quantity_sold, price, sales)\
-            VALUES(%s, %s, %s, %s, %s, %s, %s)"
-            values = (product.product_id, product.name, product.category, product.quantity, product.quantity_sold, product.price, product.sales)
-            db.insert(query, values)
+            query = f"""INSERT INTO products (
+                                            product_id, name, category,
+                                            quantity, quantity_sold,
+                                            price, sales)
+                        VALUES(
+                            '{product.product_id}', '{product.name}',
+                            '{product.category}', '{product.quantity}',
+                            '{product.quantity_sold}', '{product.price}',
+                            '{product.sales}')"""
+
+            db.insert(query)
             return
-        return "product {} already exists".format(product.name)
+        return f"product {product.name} already exists"
 
     @classmethod
     def check_if_product_exists(cls, product_name):
-        query = "SELECT * FROM products WHERE name = '{}'".format(product_name)
+        query = f"SELECT * FROM products WHERE name = '{product_name}'"
         return db.fetch_one(query)
 
     @classmethod
     def fetch_product_by_name(cls, product_name):
-        query = "SELECT * FROM products WHERE name = '{}'".format(product_name)
+        query = f"SELECT * FROM products WHERE name = '{product_name}'"
         product = db.fetch_one(query)
         if product is None:
-            return 'product ' + product_name + ' does not exist'
+            return f"product {product_name} does not exist"
         return product
 
     @classmethod
     def fetch_product_by_id(cls, product_id):
-        query = "SELECT * FROM products WHERE product_id = '{}'".format(product_id)
+        query = f"SELECT * FROM products WHERE product_id = '{product_id}'"
         product = db.fetch_one(query)
         if product is None:
-            return 'product with ID ' + str(product_id) + ' doesnot exist'
+            return f"product with ID {product_id} doesnot exist"
         return product
 
     @classmethod
@@ -48,12 +55,14 @@ class Controller:
     @classmethod
     def update_product(cls, **kwargs):
 
-        query = """ UPDATE products SET name = '{}',
-         category = '{}', quantity = '{}',
-         quantity_sold = '{}', price = '{}', sales = '{}' WHERE product_id = '{}'
-         """.format(kwargs['name'], kwargs['category'], kwargs['quantity'],
-                    kwargs['quantity_sold'], kwargs['price'], kwargs['sales'],
-                    kwargs['product_id'])
+        query = f"""UPDATE products SET name = '{kwargs['name']}',
+                                category = '{kwargs['category']}',
+                                quantity = '{kwargs['quantity']}',
+                                quantity_sold = '{kwargs['quantity_sold']}',
+                                price = '{kwargs['price']}',
+                                sales = '{kwargs['sales']}'
+                                WHERE product_id = '{kwargs['product_id']}'
+                """
 
         updated = db.update(query)
         if updated == 1:
@@ -62,5 +71,5 @@ class Controller:
 
     @classmethod
     def delete_product_by_id(cls, product_id):
-        query = "DELETE FROM products WHERE product_id = '{}'".format(product_id)
+        query = f"DELETE FROM products WHERE product_id = '{product_id}'"
         return db.delete(query)
--- flasky/sale/sale_controller.py
+++ flasky/sale/sale_controller.py
@@ -6,35 +6,35 @@ class Controller:
     """
     controller class for handling sale order interactions
     """
-
-    def __init__(self):
-        pass
-
     # save a new sale record
     @classmethod
     def add_sale_record(cls, sale):
-        query = "INSERT INTO sales (sale_id, attendant, products, total_product, total_amount)\
-            VALUES(%s, %s, %s, %s, %s)"
+        query = """INSERT INTO sales (sale_id, attendant,
+                                    products, total_product,
+                                    total_amount)
+                    VALUES(%s, %s, %s, %s, %s)"""
         for index, _ in enumerate(sale.products):
             sale.products[index].pop('created_timestamp')
 
         sale.products = [json.dumps(product) for product in sale.products]
-        values = (sale.sale_id, sale.attendant, sale.products, sale.total_products, sale.total_amount)
-        db.insert(query, values)
+        values = (sale.sale_id, sale.attendant,
+                  sale.products, sale.total_products,
+                  sale.total_amount)
+        db.old_string_insert(query, values)
 
     # fetch sale record by id
     @classmethod
     def fetch_sale_record(cls, sale_id):
-        query = "SELECT * FROM sales WHERE sale_id = '{}'".format(sale_id)
+        query = f"SELECT * FROM sales WHERE sale_id = '{sale_id}'"
         sale = db.fetch_one(query)
         if sale is not None:
             return sale
-        return 'sale with ID ' + str(sale_id) + ' doesnot exist'
+        return f"sale with ID {sale_id} doesnot exist"
 
     # fetch sale record by attendant
     @classmethod
     def fetch_sale_record_by_attendant(cls, attendant):
-        query = "SELECT * FROM sales WHERE attendant = '{}'".format(attendant)
+        query = f"SELECT * FROM sales WHERE attendant = '{attendant}'"
         sales = db.fetch_all(query)
         if sales is not None:
             return sales
@@ -45,7 +45,3 @@ class Controller:
     def fetch_all_sale_records(cls):
         query = "SELECT * FROM sales"
         return db.select_query(query)
-
-
-# create an instance of a sale controller class
-controller = Controller()
